39314751534c11d3347fe01181c9effbe2cd4575
Septatrivanto Wandy <septatw19@gmail.com>
2021-12-16 11:31:12 +0700

    Tambah nama angora kmlompok + tabah label di story board

--- MMS_LEC_Project/ViewController.swift
+++ MMS_LEC_Project/ViewController.swift
@@ -6,6 +6,16 @@
 //  Copyright © 2021 Septatrivanto Wandy. All rights reserved.
 //
 
+/*
+ Anggota Kelompok:
+ 1. 2301869512 - Septatrivanto Wandy
+ 2.
+ 3.
+ 4.
+ 5.
+ 
+*/
+
 import UIKit
 
 class ViewController: UIViewController {
