fac63f091d15969296b6aa5bbca3c490b13d28b8
Septatrivanto Wandy <septatw19@gmail.com>
2021-12-16 11:22:25 +0700

    Initial Commit

---
+++ MMS_LEC_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MMS_LEC_Project
+//
+//  Created by Septatrivanto Wandy on 16/12/21.
+//  Copyright © 2021 Septatrivanto Wandy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
